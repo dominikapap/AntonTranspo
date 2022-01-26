3daafbbd3ec41607355d96b1e701699fa31bd18d
dominikapap <dominikasz88@interia.pl>
2022-01-26 11:26:09 +0100

    hid sensitive info

--- transport_company/settings.py
+++ transport_company/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
-
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = Path(BASE_DIR) / 'templates'
@@ -23,10 +25,10 @@ STATIC_DIR = Path(BASE_DIR) / 'static'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mgsb^hx^5-6&lzs5*dvhpfgw2%3jg1&v2ln9cn&c-jr2mc$*qu'
+SECRET_KEY = env(‘SECRET_KEY’)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -137,6 +139,6 @@ DEFAULT_FROM_EMAIL = 'hotarubi@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hotarubiq@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhigcrlwnerqhbng'
+EMAIL_HOST_PASSWORD = env(‘EMAIL_HOST_PASSWORD’)
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
